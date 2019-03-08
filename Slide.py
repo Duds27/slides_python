7590a33214d2eda4466a9cd2c3ea98e532de9aa4
Eduardo Correa Encarnacao <eduardo.encarnacao@outlook.com>
2019-03-08 00:23:08 -0300

    Add border to images

--- Slide.py
+++ Slide.py
@@ -18,6 +18,7 @@ def show_image(path):
     cv2.namedWindow(PICTURE_TITLE, cv2.WINDOW_NORMAL)
     image = cv2.imread(path)
     image_resized = cv2.resize(image, (MAX_WIDTH, MAX_HEIGHT))
+    image_resized = cv2.copyMakeBorder(image_resized, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[20, 250, 150])
     cv2.imshow(PICTURE_TITLE, image_resized)
     cv2.resizeWindow(PICTURE_TITLE, MAX_WIDTH, MAX_HEIGHT)
     cv2.waitKey()
