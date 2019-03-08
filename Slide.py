2002f2825f24845408397f605d5cebb5edf15256
Eduardo Correa Encarnacao <eduardo.encarnacao@outlook.com>
2019-03-08 00:31:07 -0300

    Add functionality 6:exit the application only when user press the 'q' key

--- Slide.py
+++ Slide.py
@@ -10,9 +10,16 @@ MAX_HEIGHT = 720
 
 def main(argv):
     images = read_images_repository(PICTURE_BASE_PATH)
+    stop = False
     while True:
+        if stop:
+            break
         for i in range(0, images.__len__()):
             show_image(PICTURE_BASE_PATH + images[i])
+            if cv2.waitKey() & 0xFF == ord('q'):
+                stop = True
+                break
+    cv2.destroyAllWindows()
 
 
 def show_image(path):
@@ -22,8 +29,6 @@ def show_image(path):
     image_resized = cv2.copyMakeBorder(image_resized, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[20, 250, 150])
     cv2.imshow(PICTURE_TITLE, image_resized)
     cv2.resizeWindow(PICTURE_TITLE, MAX_WIDTH, MAX_HEIGHT)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
 
 
 def read_images_repository(path):
