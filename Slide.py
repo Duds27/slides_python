18f88b4ca7f6aeab5de5dc0a4aea54787c711779
Eduardo Correa Encarnacao <eduardo.encarnacao@outlook.com>
2019-03-07 23:58:19 -0300

    Add read all image files from directory

--- Slide.py
+++ Slide.py
@@ -1,8 +1,19 @@
 import sys
+from os import walk
+
+BASE_PICTURE_PATH = './pictures/'
 
 
 def main(argv):
-    pass
+    images = read_images_repository(BASE_PICTURE_PATH)
+    print(images.__str__())
+
+
+def read_images_repository(path):
+    images = []
+    for (dirpath, dirnames, filenames) in walk(path):
+        images.extend(filenames)
+    return images
 
 
 if __name__ == "__main__":
