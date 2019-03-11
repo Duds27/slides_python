31041d2224e67d537d5a0df6ab384098f28bf9cc
Eduardo Correa Encarnacao <eduardo.encarnacao@outlook.com>
2019-03-11 20:45:42 -0300

    Fix error of functionality 1: change the images every 2 seconds

--- Slide.py
+++ Slide.py
@@ -1,5 +1,6 @@
 import sys
 import cv2
+import time
 from os import walk
 
 PICTURE_BASE_PATH = './pictures/'
@@ -14,21 +15,48 @@ def main(argv):
     while True:
         if stop:
             break
-        for i in range(0, images.__len__()):
-            show_image(PICTURE_BASE_PATH + images[i])
-            if cv2.waitKey(2000) & 0xFF == ord('q'):
-                stop = True
-                break
+        for i in range(0, images.__len__() - 1):
+            print("Picture {} - {}".format(i.__str__(), images[i]))
+            if i == images.__len__() - 1:
+                if show_image(PICTURE_BASE_PATH + images[images.__len__() - 1], PICTURE_BASE_PATH + images[0]):
+                    stop = True
+                    break
+            else:
+                if show_image(PICTURE_BASE_PATH + images[i], PICTURE_BASE_PATH + images[i + 1]):
+                    stop = True
+                    break
     cv2.destroyAllWindows()
 
 
-def show_image(path):
+def show_image(path_image_1, path_image_2):
     cv2.namedWindow(PICTURE_TITLE, cv2.WINDOW_NORMAL)
-    image = cv2.imread(path)
-    image_resized = cv2.resize(image, (MAX_WIDTH, MAX_HEIGHT))
-    image_resized = cv2.copyMakeBorder(image_resized, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[20, 250, 150])
-    cv2.imshow(PICTURE_TITLE, image_resized)
-    cv2.resizeWindow(PICTURE_TITLE, MAX_WIDTH, MAX_HEIGHT)
+    image_1 = cv2.imread(path_image_1)
+    image_resized_1 = cv2.resize(image_1, (MAX_WIDTH, MAX_HEIGHT))
+    image_resized_1 = cv2.copyMakeBorder(image_resized_1, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[20, 250, 150])
+    image_2 = cv2.imread(path_image_2)
+    image_resized_2 = cv2.resize(image_2, (MAX_WIDTH, MAX_HEIGHT))
+    image_resized_2 = cv2.copyMakeBorder(image_resized_2, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[20, 250, 150])
+    # cv2.imshow(PICTURE_TITLE, image_resized_1)
+    return blending_fade(image_resized_1, image_resized_2)
+
+
+def stop_program():
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        return True
+    return False
+
+
+def blending_fade(first_image, second_image):
+    for i in range(1, 42):
+        fadein = i / 42.0
+        dst = cv2.addWeighted(first_image, 1 - fadein, second_image, fadein, 0)
+        cv2.imshow(PICTURE_TITLE, dst)
+        if stop_program():
+            return True
+        # print(fadein)
+        time.sleep(0.05)
+        cv2.resizeWindow(PICTURE_TITLE, MAX_WIDTH, MAX_HEIGHT)
+    return False
 
 
 def read_images_repository(path):
